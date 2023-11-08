e52ef0f38ac7a2599a8b50cbbcf10c14d5a45c72
Matthew Jonat <matthew.jonat@gmail.com>
2023-11-08 17:08:13 +0100

    created product card component. products are being pulled in from external api and displayed as a list on main page.

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { 
     Input, 
     InputField,
     InputSlot,
     InputIcon,
-    SearchIcon
+    SearchIcon,
+    VStack,
+    ScrollView
 } from '@gluestack-ui/themed';
 
 import Container from '../../components/Container';
+import ProductCard from '../../components/Product';
+
+import type { Product } from './types';
 
 const HomeScreen = () => {
+    const [products, setProducts] = useState<Array<Product>>([]);
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+    const getProducts = async () => {
+        try {
+            const response = await axios.get('https://fakestoreapi.com/products');
+
+            setProducts(response.data);
+        } catch (error) {
+            console.log('error: ', error);
+        }
+    }
+
     return (
         <Container>
-            <Input 
-                variant="rounded"
-                size="md"
-            >
-                <InputSlot>
-                    <InputIcon as={SearchIcon} color="#aaa" size="md" marginLeft={16} />
-                </InputSlot>
-                <InputField placeholder="Search..." />
-            </Input>
+            <ScrollView>
+                <Input 
+                    variant="rounded"
+                    size="md"
+                    mb="$4"
+                >
+                    <InputSlot>
+                        <InputIcon as={SearchIcon} color="#aaa" size="md" marginLeft={16} />
+                    </InputSlot>
+                    <InputField placeholder="Search..." />
+                </Input>
+                <VStack>
+                    {products.map((product) => (
+                        <ProductCard 
+                            key={product.id}
+                            title={product.title}
+                            category={product.category}
+                            price={product.price}
+                            image={product.image}
+                            rating={product.rating}
+                        />
+                    ))}
+                </VStack>
+            </ScrollView>
         </Container>
     );
 };
